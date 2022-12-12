caaaf3d3fc269cc43d85003ef4962610be89e8d1
Adam Patterson <adam@adamrt.com>
2022-12-12 13:35:35 -0800

    Add Vec2, Vec3 structs and simple vector rotation functions
    These will be replaced by rotation matrices in the future.

---
+++ math.go
@@ -0,0 +1,35 @@
+package main
+
+import "math"
+
+type Vec2 struct {
+	x, y float64
+}
+
+type Vec3 struct {
+	x, y, z float64
+}
+
+func rotate_x(v Vec3, angle float64) Vec3 {
+	return Vec3{
+		x: v.x,
+		y: v.y*math.Cos(angle) - v.z*math.Sin(angle),
+		z: v.y*math.Sin(angle) + v.z*math.Cos(angle),
+	}
+}
+
+func rotate_y(v Vec3, angle float64) Vec3 {
+	return Vec3{
+		x: v.x*math.Cos(angle) - v.z*math.Sin(angle),
+		y: v.y,
+		z: v.x*math.Sin(angle) + v.z*math.Cos(angle),
+	}
+}
+
+func rotate_z(v Vec3, angle float64) Vec3 {
+	return Vec3{
+		x: v.x*math.Cos(angle) - v.y*math.Sin(angle),
+		y: v.x*math.Sin(angle) + v.y*math.Cos(angle),
+		z: v.z,
+	}
+}
