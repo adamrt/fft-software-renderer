d6d71bedb25a72c5d5ec4c7616ffe06a4574d77d
Adam Patterson <adam@adamrt.com>
2022-12-30 22:36:55 -0600

    Remove unused light code

--- light.go
+++ light.go
@@ -1,35 +1,10 @@
 package main
 
-import (
-	"math"
-)
-
 type DirectionalLight struct {
 	position Vec3
-	target   Vec3
 	color    Color
-}
-
-func NewDirectionLight(position, target Vec3) DirectionalLight {
-	return DirectionalLight{position: position, target: position}
-}
-
-func (l *DirectionalLight) direction() Vec3 {
-	return l.target.Sub(l.position)
 }
 
 type AmbientLight struct {
 	color Color
-}
-
-func applyLightIntensity(orig Color, factor float64) Color {
-	// Clamp from 0.0 to 1.0
-	factor = math.Max(0, math.Min(factor, 1.0))
-
-	return Color{
-		R: uint8(float64(orig.R) * factor),
-		G: uint8(float64(orig.G) * factor),
-		B: uint8(float64(orig.B) * factor),
-		A: orig.A,
-	}
 }
