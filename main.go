6c06ed3d656f946c5d9c3863a0bf35ffd4ce7168
Adam Patterson <adam@adamrt.com>
2023-01-27 08:58:09 -0800

    Allow user to specify the bin file as an argument

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
 const (
 	windowWidth  = 1024
 	windowHeight = 768
@@ -10,7 +16,9 @@ func main() {
 	window := NewWindow(windowWidth, windowHeight)
 	defer window.Close()
 	renderer := NewRenderer(window)
-	reader := NewReader("/home/adam/tmp/fft.bin")
+
+	path := getPath()
+	reader := NewReader(path)
 
 	engine := NewEngine(window, renderer, reader)
 	engine.setMap(49)
@@ -21,4 +29,18 @@ func main() {
 		engine.update()
 		engine.render()
 	}
+}
+
+func getPath() string {
+	// Use user specified path
+	if len(os.Args) > 1 {
+		return os.Args[1]
+	}
+
+	// Use default path
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(usr.HomeDir, "tmp", "emu", "fft.bin")
 }
